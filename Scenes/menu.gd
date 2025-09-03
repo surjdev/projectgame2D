99752200a33046ce37e75a749868f3d41b42a07e
Kasidit Boonsaner <kasidit1217@gmail.com>
2025-09-03 21:16:19 +0700

    menu path to level 1 scene

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -2,8 +2,7 @@ extends Node2D
 
 
 func _on_start_button_pressed() -> void:
-	#get_tree().change_scene_to_file("path_scene")
-	pass
+	get_tree().change_scene_to_file("res://Scenes/levels/level_01.tscn")
 
 
 func _on_exit_button_pressed() -> void:
