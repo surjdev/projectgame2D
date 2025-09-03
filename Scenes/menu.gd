bcf9f87fd254681dfa3c774491c652a211a33f05
Kasidit Boonsaner <kasidit1217@gmail.com>
2025-09-03 17:41:09 +0700

    edit ui

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 
 func _on_start_button_pressed() -> void:
-	pass # Replace with function body.
+	#get_tree().change_scene_to_file("path_scene")
+	pass
 
 
 func _on_exit_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
